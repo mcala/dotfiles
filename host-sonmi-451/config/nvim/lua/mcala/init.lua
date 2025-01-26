9fc1fd787546f6a78d3b7fd68b754dcaff55756d
Andrew McAllister <andrew@mcallister.science>
2025-01-25 19:35:52 -0500

    Adds neovim "from scratch" configuration--unfinished and unused.

--- host-sonmi-451/config/nvim/lua/mcala/init.lua
+++
@@ -1,9 +0,0 @@
-if vim.g.vscode then
-    print("Loading vscode neovim...")
-    require("mcala.vscode")
-else
-    require("mcala.remap")
-    require("mcala.options")
-    require("mcala.lazy")
-    vim.cmd("colorscheme rose-pine-dawn")
-end
