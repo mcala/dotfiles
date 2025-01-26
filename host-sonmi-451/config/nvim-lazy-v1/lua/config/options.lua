286e52f34d4026ee949806ccdaed36edec2d50a8
Andrew McAllister <andrew@mcallister.science>
2025-01-25 19:36:13 -0500

    Adds lazyvim v1 configuration, unused.

---
+++ host-sonmi-451/config/nvim-lazy-v1/lua/config/options.lua
@@ -0,0 +1,24 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+--
+
+vim.g.python3_host_prog = "/Users/mcala/anaconda3/envs/neovim/bin/python"
+vim.g.python_recommended_style = 0
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.list = true
+vim.opt.listchars = "tab:▸ ,eol:¬,trail:."
+
+vim.opt.undofile = true
+vim.opt.undodir = "/Users/mcala/.config/nvim/undofiles/"
+vim.opt.undolevels = 10000
+
+vim.opt.swapfile = true
+vim.opt.directory = "/Users/mcala/.config/nvim/swapfiles/"
+
+vim.opt.winbar = "%=%m %f"
